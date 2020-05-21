32b24dd5a012f265522a1c0f26db4f9893341ff3
Amine Yildiz <yildizamine95@gmail.com>
2020-05-21 13:45:20 +0300

    Eclipse Hibernate kullanımı ve MySQL bağlantısı

---
+++ _001_SelamHibernate/src/com/amine/Personel.java
@@ -0,0 +1,39 @@
+package com.amine;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Personel {
+
+	@Column(name="ID")
+	private int personelId;
+	@Column(name="ADÝ")
+	private String personeladi;
+	@Column(name="SOYADI")
+	private String personelsoyadi;
+	
+	@Id
+	public int getPersonelId() {
+		return personelId;
+	}
+	
+	public void setPersonelId(int personelId) {
+		this.personelId = personelId;
+	}
+
+	public String getPersoneladi() {
+		return personeladi;
+	}
+	public void setPersoneladi(String personeladi) {
+		this.personeladi = personeladi;
+	}
+	public String getPersonelsoyadi() {
+		return personelsoyadi;
+	}
+	public void setPersonelsoyadi(String personelsoyadi) {
+		this.personelsoyadi = personelsoyadi;
+	}
+	
+}
