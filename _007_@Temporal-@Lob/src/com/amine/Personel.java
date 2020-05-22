e8054f193f3dfe608b06f82b130cd222efcc7a81
Amine Yildiz <yildizamine95@gmail.com>
2020-05-22 18:01:44 +0300

    Annotation @Temporal ve @Lob Kullanımı

---
+++ _007_@Temporal-@Lob/src/com/amine/Personel.java
@@ -0,0 +1,72 @@
+package com.amine;
+
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="CALÝSANLAR")//Veritabaný tablo adý.
+public class Personel {
+
+	@Column(name="ID") //Veritabanýndaki tablodaki bir kolon adý.
+	private int personelId;
+	@Column(name="ADI")
+	@Basic // bu alan veritabanýnda mutlakabulunacak.
+	private String personeladi;
+	
+	private String personelsoyadi;
+	
+	
+	@Temporal(TemporalType.DATE)
+	private Date personelgiristarihi;
+	
+	@Lob
+	@Column(name="ADRES")
+	public Date getPersonelgiristarihi() {
+		return personelgiristarihi;
+	}
+
+	public void setPersonelgiristarihi(Date personelgiristarihi) {
+		this.personelgiristarihi = personelgiristarihi;
+	}
+
+	public String getPersonleadres() {
+		return personleadres;
+	}
+
+	public void setPersonleadres(String personleadres) {
+		this.personleadres = personleadres;
+	}
+	private String personleadres;
+	
+	@Id
+	public int getPersonelId() {
+		return personelId;
+	}
+	
+	public void setPersonelId(int personelId) {
+		this.personelId = personelId;
+	}
+
+	public String getPersoneladi() {
+		return personeladi;
+	}
+	public void setPersoneladi(String personeladi) {
+		this.personeladi = personeladi;
+	}
+	public String getPersonelsoyadi() {
+		return personelsoyadi;
+	}
+	public void setPersonelsoyadi(String personelsoyadi) {
+		this.personelsoyadi = personelsoyadi;
+	}
+	
+}
